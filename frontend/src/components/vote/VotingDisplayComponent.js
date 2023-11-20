10a55912b2d0c5390fec49e145b5fa0163c46995
vscoding1988 <vscoding1988@gmail.de>
2023-11-20 17:09:18 +0100

    added pie chart for voting result

--- frontend/src/components/vote/VotingDisplayComponent.js
+++ frontend/src/components/vote/VotingDisplayComponent.js
@@ -1,17 +1,15 @@
+import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
+import {Pie} from 'react-chartjs-2';
+
 /**
  *
  * @param votes {Array<Object>} {vote:string,name:string,voted:boolean}
  * @returns {JSX.Element}
  * @constructor
  */
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 function VotingDisplayComponent({votes}) {
-
-  function renderVotes() {
-    let groupedVotes = groupVotes();
-
-    return Object.keys(groupedVotes).map(vote => (
-            <div key={vote}>Vote {vote}: {groupedVotes[vote]}</div>))
-  }
 
   /**
    * Group votes based of the vote to a list, sorted by vote count
@@ -30,9 +28,34 @@ function VotingDisplayComponent({votes}) {
     return lookup;
   }
 
+  /**
+   * Get config for the PIE chart
+   * @returns {{datasets: [{backgroundColor: string[], data: unknown[]}], labels: string[]}}
+   */
+  function getData() {
+    let groupedVotes = groupVotes();
+
+    return {
+      labels: Object.keys(groupedVotes),
+      datasets: [
+        {
+          data: Object.values(groupedVotes),
+          backgroundColor: [
+            'rgba(58,250,0,0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+        }
+      ]
+    }
+  }
+
   return (
           <div className="voting-container">
-            {renderVotes()}
+            <Pie data={getData()}/>
           </div>
 
   );
--- readme.md
+++ readme.md
@@ -1,9 +1,18 @@
 # Planing poker 
 
-## TODO
+## Project Plan
 
-- [ ] create session
-- 
+- [x] create session
+- [x] styling of home page
+- [x] styling of voting view
+- [x] styling of vote result (create a pie chart)
+- [ ] next vote function
+- [ ] display vote function
+- [ ] implement icons in voting screen
+- [ ] allow "enter as spectator" (checkbox, when clicked displays text which describes the role)
+- [ ] create change of facilitator
+
+
 ## Creation of a new session
 
 ![creation-new-session.png](docs/assets/creation-new-session.png)
